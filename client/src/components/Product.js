1f7421d8b8f189c61a2bcc4ea28112a05d17d3ee
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-09-12 17:39:30 -0700

    changed titles

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -17,7 +17,7 @@ function Product({product}) {
             <Rating value={product.rating} text={` ${product.numReviews} reviews`} color='yellow'></Rating>
         </Card.Text>
 
-        <Card.Text as='h3'>$ {product.price}</Card.Text>
+        <Card.Text as='h3'>$ {product.price} per hour</Card.Text>
         </Card.Body>
     </Card>
   )
